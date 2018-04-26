ee5c425f8f27520c6debed33282f91087a0dcde0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-04-26 22:27:09 +0200

    Add Results.create_run/1

--- lib/results.ex
+++ lib/results.ex
@@ -1,2 +1,9 @@
 defmodule Brittle.Results do
+  alias Brittle.{Repo, Run}
+
+  def create_run(attributes \\ %{}) do
+    %Run{}
+    |> Run.changeset(attributes)
+    |> Repo.insert()
+  end
 end
--- test/results_test.exs
+++ test/results_test.exs
@@ -1,7 +1,21 @@
 defmodule Brittle.ResultsTest do
   use ExUnit.Case
+  alias Brittle.{Results, Run}
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Brittle.Repo)
+  end
+
+  test "create_run/1 creates a run" do
+    assert {:ok, %Run{} = run} =
+             Results.create_run(%{
+               test_count: 2,
+               failure_count: 1,
+               duration: 42
+             })
+
+    assert run.test_count == 2
+    assert run.failure_count == 1
+    assert run.duration == 42
   end
 end
