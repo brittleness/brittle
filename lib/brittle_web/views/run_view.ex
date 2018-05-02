0d2c3a0ad5b4c6b7fc97037a1f4d1f4a03eadb8b
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-02 19:26:57 +0200

    Add Brittle.View.RunView.human_duration/1

--- lib/brittle_web/views/run_view.ex
+++ lib/brittle_web/views/run_view.ex
@@ -5,4 +5,8 @@ defmodule Brittle.Web.RunView do
   def short_revision(%Run{revision: revision}) do
     String.slice(revision, 0..6)
   end
+
+  def human_duration(%Run{duration: duration}) do
+    "#{Float.round(duration / 1_000_000, 2)} s"
+  end
 end
--- test/brittle_web/views/run_view_test.exs
+++ test/brittle_web/views/run_view_test.exs
@@ -6,4 +6,9 @@ defmodule Brittle.Web.RunViewTest do
     assert %Run{revision: "df54993999a5b340c8d3949e526ae91dba09a351"}
            |> RunView.short_revision() == "df54993"
   end
+
+  test "human_duration/1 returns the duration in seconds" do
+    assert %Run{duration: 20_713_727}
+           |> RunView.human_duration() == "20.71 s"
+  end
 end
