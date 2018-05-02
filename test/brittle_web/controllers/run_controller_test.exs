76be1719f5a8a1a1be097c73e47fca7a4fe9ba5e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-02 20:26:52 +0200

    Show inserted_at on RunController index

--- test/brittle_web/controllers/run_controller_test.exs
+++ test/brittle_web/controllers/run_controller_test.exs
@@ -13,5 +13,6 @@ defmodule Brittle.Web.RunControllerTest do
     assert response =~ "master"
     assert response =~ ~s(<td><abbr title="df54993999a5b340c8d3949e526ae91dba09a351">df54993</abbr></td>)
     assert response =~ ~s(<td><time datetime="20.714 s">20.71 s</time></td>)
+    assert response =~ ~s(<td><time datetime="2018-05-01T21:33:32.163433Z">2018-05-01 21:33:32 <small>(UTC\)</small></time></td>)
   end
 end
