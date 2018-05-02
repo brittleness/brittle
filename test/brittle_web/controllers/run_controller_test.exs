e8a14de3b3e7b0b9e2b44e25a85fc025ac457f01
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-02 22:28:31 +0200

    Show test, exclude and failue count on RunController index

--- test/brittle_web/controllers/run_controller_test.exs
+++ test/brittle_web/controllers/run_controller_test.exs
@@ -12,6 +12,9 @@ defmodule Brittle.Web.RunControllerTest do
     assert response =~ "Alices-MBP.fritz.box"
     assert response =~ "master"
     assert response =~ ~s(<td><abbr title="df54993999a5b340c8d3949e526ae91dba09a351">df54993</abbr></td>)
+    assert response =~ ~s(<td>617</td>)
+    assert response =~ ~s(<td>4</td>)
+    assert response =~ ~s(<td>0</td>)
     assert response =~ ~s(<td><time datetime="20.714 s">20.71 s</time></td>)
     assert response =~ ~s(<td><time datetime="2018-05-01T21:33:32.163433Z">2018-05-01 21:33:32 <small>(UTC\)</small></time></td>)
   end
