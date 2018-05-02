d7c12820a7acdee763067a8001e1f4fb952e8e50
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-02 23:47:02 +0200

    Use changesets in fixtures, split up fixture(:suite)

--- lib/fixtures.ex
+++ lib/fixtures.ex
@@ -1,16 +1,21 @@
 defmodule Brittle.Fixtures do
   alias Brittle.{Repo, Suite, Run}
 
-  def fixture(:suite) do
-    %Suite{
+  def attributes(:suite) do
+    %{
       name: "phoenix",
       inserted_at: ~N[2018-05-01 21:32:21.097299],
       updated_at: ~N[2018-05-01 21:32:21.097312]
     }
   end
 
+  def fixture(:suite) do
+    Suite.changeset(%Suite{}, attributes(:suite))
+  end
+
   def fixture(:run) do
-    %Run{
+    %Run{}
+    |> Run.changeset(%{
       branch: "master",
       digest: "5e40eb035846ae6e762cbd22e65c34f3",
       dirty: true,
@@ -20,10 +25,10 @@ defmodule Brittle.Fixtures do
       hostname: "Alices-MBP.fritz.box",
       revision: "df54993999a5b340c8d3949e526ae91dba09a351",
       test_count: 617,
-      suite: fixture(:suite),
+      suite: attributes(:suite),
       inserted_at: ~N[2018-05-01 21:33:32.163433],
       updated_at: ~N[2018-05-01 21:33:32.163448]
-    }
+    })
   end
 
   def fixture!(name) do
