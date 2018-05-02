0bf4f27f9cd7277edd9c6793772ec4e92aac18b5
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-02 19:21:58 +0200

    Add Brittle.View.RunView.short_revision/1

--- lib/brittle_web/views/run_view.ex
+++ lib/brittle_web/views/run_view.ex
@@ -1,3 +1,8 @@
 defmodule Brittle.Web.RunView do
   use Brittle.Web, :view
+  alias Brittle.Run
+
+  def short_revision(%Run{revision: revision}) do
+    String.slice(revision, 0..6)
+  end
 end
--- test/brittle_web/views/run_view_test.exs
+++ test/brittle_web/views/run_view_test.exs
@@ -1,3 +1,9 @@
 defmodule Brittle.Web.RunViewTest do
   use Brittle.Web.ConnCase, async: true
+  alias Brittle.{Run, Web.RunView}
+
+  test "short_revision/1 returns the short revision" do
+    assert %Run{revision: "df54993999a5b340c8d3949e526ae91dba09a351"}
+           |> RunView.short_revision() == "df54993"
+  end
 end
