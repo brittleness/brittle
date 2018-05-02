2e7dc957e15bd97169a9ceeec07b7caa14916cfb
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-02 19:59:43 +0200

    Show short revision and duration on RunController index

--- test/brittle_web/controllers/run_controller_test.exs
+++ test/brittle_web/controllers/run_controller_test.exs
@@ -11,6 +11,7 @@ defmodule Brittle.Web.RunControllerTest do
     assert response =~ "Runs for phoenix"
     assert response =~ "Alices-MBP.fritz.box"
     assert response =~ "master"
-    assert response =~ ~s(df54993999a5b340c8d3949e526ae91dba09a351)
+    assert response =~ ~s(<td><abbr title="df54993999a5b340c8d3949e526ae91dba09a351">df54993</abbr></td>)
+    assert response =~ ~s(<td><time datetime="20.714 s">20.71 s</time></td>)
   end
 end
