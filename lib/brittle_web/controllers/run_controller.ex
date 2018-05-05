1028f2c7a75cd402b97f59846c3de67b289863dc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-05 11:37:14 +0200

    RunController index orders runs by finished_at, newest first

--- lib/brittle_web/controllers/run_controller.ex
+++ lib/brittle_web/controllers/run_controller.ex
@@ -1,11 +1,13 @@
 defmodule Brittle.Web.RunController do
   require Ecto.Query
   use Brittle.Web, :controller
-  alias Brittle.{Repo, Suite}
+  alias Brittle.{Repo, Suite, Run}
 
   def index(conn, %{"suite_id" => suite_id}) do
+    run_query = Ecto.Query.order_by(Run, desc: :inserted_at)
+
     suite = Suite
-    |> Ecto.Query.preload(:runs)
+    |> Ecto.Query.preload(runs: ^run_query)
     |> Repo.get(suite_id)
 
     render conn, "index.html", suite: suite
--- test/brittle_web/controllers/run_controller_test.exs
+++ test/brittle_web/controllers/run_controller_test.exs
@@ -2,21 +2,34 @@ defmodule Brittle.Web.RunControllerTest do
   use Brittle.Web.ConnCase
   import Brittle.Fixtures
 
-  test "GET /", %{conn: conn} do
-    run = fixture!(:run)
+  setup %{conn: conn} do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Brittle.Repo)
 
-    conn = get conn, "/suites/#{run.suite_id}/runs"
-    response = html_response(conn, 200)
+    [run | _] = [fixture!(:run), fixture!(:failed_run)]
+    conn = get(conn, "/suites/#{run.suite_id}/runs")
 
-    assert response =~ "<td>✓</td>"
-    assert response =~ "Runs for phoenix"
-    assert response =~ "Alices-MBP.fritz.box"
-    assert response =~ "master"
-    assert response =~ ~s(<td><abbr title="df54993999a5b340c8d3949e526ae91dba09a351">df54993</abbr></td>)
-    assert response =~ ~s(<td>617</td>)
-    assert response =~ ~s(<td>4</td>)
-    assert response =~ ~s(<td>0</td>)
-    assert response =~ ~s(<td><time datetime="20.714 s">20.71 s</time></td>)
-    assert response =~ ~s(<td><time datetime="2018-05-01T21:33:32.163448Z">2018-05-01 21:33:32Z</time></td>)
+    [body: html_response(conn, 200)]
+  end
+
+  test "shows all runs for a suite", %{body: body} do
+    assert body =~ "<td>✓</td>"
+    assert body =~ "Runs for phoenix"
+    assert body =~ "Alices-MBP.fritz.box"
+    assert body =~ "master"
+
+    assert body =~
+             ~s(<td><abbr title="df54993999a5b340c8d3949e526ae91dba09a351">df54993</abbr></td>)
+
+    assert body =~ ~s(<td>617</td>)
+    assert body =~ ~s(<td>4</td>)
+    assert body =~ ~s(<td>0</td>)
+    assert body =~ ~s(<td><time datetime="20.714 s">20.71 s</time></td>)
+
+    assert body =~
+             ~s(<td><time datetime="2018-05-01T21:33:32.163448Z">2018-05-01 21:33:32Z</time></td>)
+  end
+
+  test "orders the runs by their finished_at dates, newest first", %{body: body} do
+    assert Regex.scan(~r[✓|✗], body) == [~w(✗), ~w(✓)]
   end
 end
