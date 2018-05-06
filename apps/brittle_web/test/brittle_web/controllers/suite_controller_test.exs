55aa40441bb719634fa5c4737d6f4379ac3a09fe
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-06 11:20:56 +0200

    Add regression test for links on SuiteController index

--- apps/brittle_web/test/brittle_web/controllers/suite_controller_test.exs
+++ apps/brittle_web/test/brittle_web/controllers/suite_controller_test.exs
@@ -2,18 +2,14 @@ defmodule Brittle.Web.SuiteControllerTest do
   use Brittle.Web.ConnCase
   import Brittle.Fixtures
 
-  setup do
+  test "lists all suites", %{conn: conn} do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Brittle.Repo)
 
-    fixture!(:suite)
-    :ok
-  end
-
-  test "GET /", %{conn: conn} do
+    %{id: id} = fixture!(:suite)
     conn = get conn, "/"
     response = html_response(conn, 200)
 
     assert response =~ "Suites"
-    assert response =~ "phoenix"
+    assert response =~ ~s(<a href="/suites/#{id}/runs">phoenix</a>)
   end
 end
