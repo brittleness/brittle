21bd3ea359743f9d8a0cfa8622fc42f7abbdb042
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-17 20:44:30 +0200

    Show the run summary on RunController show

--- apps/brittle_web/test/brittle_web/controllers/run_controller_test.exs
+++ apps/brittle_web/test/brittle_web/controllers/run_controller_test.exs
@@ -66,6 +66,29 @@ defmodule Brittle.Web.RunControllerTest do
       assert body =~ "<h2>Run ##{run_id}</h2>"
     end
 
+    test "shows the run's summary", %{
+      body: body,
+      run: %{id: run_id, suite_id: suite_id}
+    } do
+      assert body =~ ~s(<td class="status_icon">✓</td>)
+
+      assert body =~
+               ~s(<a href="/suites/#{suite_id}/runs/#{run_id}"># #{run_id}</a>)
+
+      assert body =~ "master"
+
+      assert body =~
+               ~s(<td><abbr title="df54993999a5b340c8d3949e526ae91dba09a351">df54993</abbr></td>)
+
+      assert body =~ ~s(<td>617</td>)
+      assert body =~ ~s(<td>4</td>)
+      assert body =~ ~s(<td>0</td>)
+      assert body =~ ~s(<time datetime="20.714 s">20.71 s</time>)
+
+      assert body =~
+               ~s(<td><time datetime="2018-05-01T21:33:32.163448Z">2018-05-01 21:33:32.163448Z</time></td>)
+    end
+
     test "shows all results for a run", %{body: body} do
       assert body =~ ~s(<td class="status_icon">✓</td>)
       assert body =~ ~s(Elixir.<wbr>ExampleTest)
@@ -76,11 +99,21 @@ defmodule Brittle.Web.RunControllerTest do
     end
 
     test "orders the results by their statuses, failures first", %{body: body} do
-      assert Regex.scan(~r[✓|✗], body) == [~w(✗), ~w(✓), ~w(✓)]
+      assert ~r[✓|✗]
+             |> Regex.scan(body)
+             |> Enum.slice(1..-1) == [~w(✗), ~w(✓), ~w(✓)]
     end
 
-    test "after statuses, orders results by their durations, slowest first", %{body: body} do
-      assert Regex.scan(~r[\d+.\d{3} s], body) == [["6.314 s"], ["20.314 s"], ["0.314 s"]]
+    test "after statuses, orders results by their durations, slowest first", %{
+      body: body
+    } do
+      assert ~r[\d+.\d{3} s]
+             |> Regex.scan(body)
+             |> Enum.slice(1..-1) == [
+               ["6.314 s"],
+               ["20.314 s"],
+               ["0.314 s"]
+             ]
     end
 
     test "shows a duration bar for each result", %{body: body} do
